bf08be24288b97a5eb0bd9d193df217084836cb7
etaaquarids <108239813+etaaquarids@users.noreply.github.com>
2023-07-09 19:14:36 +0800

    LzyServer

--- LzyServer/LzyServer.cpp
+++ LzyServer/LzyServer.cpp
@@ -10,47 +10,39 @@
 auto logger = Lzy::Log::Logger<Lzy::Log::Outter::Console>::get_instance();
 using namespace std::literals::string_literals;
 
-Lzy::Coroutine::Task<bool> Echo(SOCKET socket, std::array<char, 952> buffer) {
-    logger.info("Echo recvd:\n", buffer.data());
-    while (true) {
-        
-        if (auto error = co_await Lzy::Async::send(socket, buffer); error != 0) {
-            break;
-        }
-        if (auto res = co_await Lzy::Async::recv(socket, buffer); res == -1) {
-            break;
-        }
-        //logger.info("recvd:\n", buffer.data());
-        if (buffer.data()[0] == 'q') {
-            break;
-        }
-    }
-    //co_await Lzy::Async::recv(socket, buffer);
-    co_return true;
-}
+Lzy::Http::ResponseHead OKHEAD{ .version{1.1}, .statusCode{200}, .comment{"OK"} };
+Lzy::Http::ResponseHead NOTFOUNDHEAD{ .version{1.1}, .statusCode{404}, .comment{"NOTFOUND"} };
 
 Lzy::Coroutine::Task<bool> HTTP(SOCKET socket, std::span<char> buffer) {
     Lzy::Http::Request request;
     Lzy::Http::Response response;
-   
-    
+
+
     if (!request.parser({ buffer.begin(), buffer.end() })) {
         logger.info("not Http");
         co_return false;
     }
     response.headers = { "Access-Control-Allow-Origin: *", "Access-Control-Max-Age: 1728000" };
-    response.head.version = 1.1;
-    response.head.statusCode = 200;
-    response.head.comment = "ok";
-    response.content = request.content;
+   
+
+    std::filesystem::path path = std::filesystem::path("..\\HTML");
+    path /= request.head.url.substr(1);
+    if(std::filesystem::exists(path))
+    {
+        response.head = OKHEAD;
+        std::ifstream file(path);
+        response.content.resize(std::filesystem::file_size(path));
+        file.read(response.content.data(), std::filesystem::file_size(path));
+    }
+    else {
+        logger.info("error page:", path.string());
+        response.head = NOTFOUNDHEAD;
+    }
+    
     auto response_buffer = response.to_string();
     if (auto error = co_await Lzy::Async::send(socket, response_buffer); error != 0) {
         co_return false;
     }
-    co_return true;
-}
-
-Lzy::Coroutine::Task<bool> RPC(SOCKET socket, std::span<char> buffer) {
     co_return true;
 }
 
@@ -63,36 +55,23 @@ Lzy::Coroutine::Task<> socket_listener(std::atomic<size_t>& numOfAcceptor) {
     while (true)
     {
         numOfAcceptor++;
-        //logger.info("now accept = ", numOfAcceptor);
         auto socket = co_await Lzy::Async::accept();
         numOfAcceptor--;
-        //logger.info("now accept = ", numOfAcceptor);
         if (numOfAcceptor == 0) {
             logger.info("listening unit added");
             auto task = socket_listener(numOfAcceptor);
             task._handle.resume();
         }
-
         logger.info("socket =", socket);
 
         if (auto res = co_await Lzy::Async::recv(socket, buffer); res == -1) {
             break;
         }
         else {
-       
-            std::vector<Lzy::Coroutine::Task<bool>> tasks;
-            tasks.reserve(2);
-            tasks.emplace_back(HTTP(socket, buffer));
-            tasks.emplace_back(Echo(socket, buffer));
-            for (bool finished; auto & task : tasks) {
-                finished = co_await Lzy::Coroutine::Join(std::move(task));
-                if (finished) break;
-            }
-              
+            co_await Lzy::Coroutine::Join(HTTP(socket, buffer));
         }
        
         closesocket(socket);
-        logger.info("socket closed");
     }
 
     logger.info("listening unit end");
@@ -103,10 +82,10 @@ Lzy::Coroutine::Task<> socket_listener(std::atomic<size_t>& numOfAcceptor) {
 int main()
 {
     using namespace Lzy::Log::Formatter;
-    logger["console"].first.level = Lzy::Log::Level::error | Lzy::Log::Level::info | Lzy::Log::Level::error;
+    logger["console"].first.level = Lzy::Log::Level::error | Lzy::Log::Level::info | Lzy::Log::Level::warning;
     logger.info("server started");
     Lzy::Async::NetSchdeler<int>::get_instance().run();
-    //test();
+
     std::atomic<size_t> numOfClient = 0;
     std::atomic<size_t> numOfAcceptor{0};
 
